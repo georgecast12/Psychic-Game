c5bdd8f7ea6d103da58c75397ff1c7aa7d7479aa
GeorgeCast <georgecast12@gmail.com>
2019-03-31 21:18:13 -0700

    simplified js code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,8 +7,7 @@ var losses = 0;
 var guessesLeft = 9;
 var userGuess = " ";
 var computerGuess = " ";
-var guesses
-var letterOptions = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+var guesses = " ";
 var computerOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 //Create variables that reference div to HMTL
@@ -23,51 +22,34 @@ var computerGuess = computerOptions[Math.floor(Math.random() * computerOptions.l
 console.log(computerGuess)
 
 
-document.onkeyup = function (event) {
+document.onkeydown = function (event) {
 
     // Determines which key was pressed by user
     var userGuess = event.key;
     console.log(userGuess)
+
+    document.getElementById("loss-Record").innerHTML = "Losses: " + losses;
+    document.getElementById("wins-Record").innerHTML = "Wins: " + wins;
+    document.getElementById("attempts-Countdown").innerHTML = "Guesses left: " + guessesLeft;
+    document.getElementById("attempts-Record").innerHTML = "Your Guesses So Far:" + userGuess;
 
     // Only run the following code block if the user presses "r" or "p" or "s".
     if (userGuess === computerGuess) {
         wins++;
         guessesLeft = 9;
         computerGuess = computerOptions[Math.floor(Math.random() * computerOptions.length)];
-        document.getElementById("loss-Record").innerHTML = "Losses: " + losses;
-        document.getElementById("wins-Record").innerHTML = "Wins: " + wins++;
-        document.getElementById("attempts-Countdown").innerHTML = "Guesses left:" + guessesLeft;
-
-
-        console.log(wins)
 
     }
 
     else if ((userGuess !== computerGuess) && (guessesLeft < 10) && (guessesLeft > 0)) {
         guessesLeft--;
-        document.getElementById("attempts-Countdown").innerHTML = "Guesses left: " + guessesLeft;
-        document.getElementById("attempts-Record").innerHTML = "Your Guesses So Far:  " + userGuess + ",";
-        document.getElementById("loss-Record").innerHTML = "Losses: " + losses;
-        document.getElementById("wins-Record").innerHTML = "Wins: " + wins;
+
     }
 
     if (userGuess !== computerGuess && guessesLeft === 0) {
         losses++;
         guessesLeft = 9;
-        document.getElementById("attempts-Countdown").innerHTML = "Guesses left: " + guessesLeft;
-        document.getElementById("loss-Record").innerHTML = "Losses: " + losses;
-        document.getElementById("attempts-Record").innerHTML = "Your Guesses So Far:";
-        document.getElementById("wins-Record").innerHTML = "Wins: " + wins;
-        console.log(losses)
-
-
     }
-
-
-
-
-
-
 
 
 };
